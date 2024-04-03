6edac82e50f3804dbb2115cafddb3c8372636117
Mebanseibor <mebanseiborlm@gmail.com>
2024-04-03 13:01:05 +0530

    Code Cleanup:     -Deleted duplicate funnction     -Removed irrelevant else part

--- Lua Scripts/Analyse Contacts.lua
+++ Lua Scripts/Analyse Contacts.lua
@@ -1,45 +1,32 @@
 function analyse_contacts(i_side)
-        local function is_table_empty(lf_table)
-            if #lf_table==0 then return true
-            else return false end
-        end
-        local function print_contact_info(lf_contact)
-                print("Contact Name: "..lf_contact.name)
-                print("Contact Observer Posture: "..lf_contact.observer_posture)
-                print("Contact Type: "..tostring(lf_contact.type))
-                print("Contact Type Description: "..tostring(lf_contact.type_description))
-                print("Contact Speed: "..tostring(lf_contact.speed).." knots")
-                print("Contact Altitude: "..tostring(lf_contact.altitude).." meters")
-                print("Contact Last Detected age: "..tostring(lf_contact.lastDetections[1].age).." seconds ago")
-                print("Contact Classification  Level: "..tostring(lf_contact.classificationlevel))
-                print("Contact Filter Out: "..tostring(lf_contact.FilterOut))
-                print("")
-        end
-        local function is_table_empty(lf_table)
-            if #lf_table==0 then return true
-            else return false end
-        end
-        local function print_contact_info(lf_contact)
-                print("Contact Name: "..lf_contact.name)
-                print("Contact Observer Posture: "..lf_contact.observer_posture)
-                print("Contact Type: "..tostring(lf_contact.type))
-                print("Contact Type Description: "..tostring(lf_contact.type_description))
-                print("Contact Speed: "..tostring(lf_contact.speed).." knots")
-                print("Contact Altitude: "..tostring(lf_contact.altitude).." meters")
-                print("Contact Last Detected age: "..tostring(lf_contact.lastDetections[1].age).." seconds ago")
-                print("Contact Classification  Level: "..tostring(lf_contact.classificationlevel))
-                print("Contact Filter Out: "..tostring(lf_contact.FilterOut))
-                print("")
-        end
-        local _contacts_list = ScenEdit_GetContacts(i_side)
-        if is_table_empty(_contacts_list) then
-            print("No Contacts")
-            return
-        else
-            for k,_contact in pairs(_contacts_list) do
-                --
-            end
-        end
+    local function is_table_empty(lf_table)
+        if #lf_table==0 then return true
+        else return false end
+    end
+    local function print_contact_info(lf_contact)
+        print("Contact Name: "..lf_contact.name)
+        print("Contact Observer Posture: "..lf_contact.observer_posture)
+        print("Contact Type: "..tostring(lf_contact.type))
+        print("Contact Type Description: "..tostring(lf_contact.type_description))
+        print("Contact Speed: "..tostring(lf_contact.speed).." knots")
+        print("Contact Altitude: "..tostring(lf_contact.altitude).." meters")
+        print("Contact Last Detected age: "..tostring(lf_contact.lastDetections[1].age).." seconds ago")
+        print("Contact Classification  Level: "..tostring(lf_contact.classificationlevel))
+        print("Contact Filter Out: "..tostring(lf_contact.FilterOut))
+        print("")
+    end
+    local function is_table_empty(lf_table)
+        if #lf_table==0 then return true
+        else return false end
+    end
+    local _contacts_list = ScenEdit_GetContacts(i_side)
+    if is_table_empty(_contacts_list) then
+        print("No Contacts")
+        return
+    end
+    for k,_contact in pairs(_contacts_list) do
+        -----Enter Tasks here-----
+    end
 end
 print("-----START-----")
 local v_side = "Blue"
