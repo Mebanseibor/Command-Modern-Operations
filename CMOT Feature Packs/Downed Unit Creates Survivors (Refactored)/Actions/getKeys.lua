6a5acceee1b527e90d13e60761f10d5d35deb4ad
Mebanseibor Lyngdoh <mebanseiborlm@gmail.com>
2025-02-28 01:09:58 +0530

    Updated RunMe and getKeys Lua scripts - Integrated table functions for inputting decision for addition or   removal of the package - SAR side is not added automatically by the script instead of relying   on the player to create and then select it during input phase - Sides are now tracked by their corresponding guid instead of their name
    Notes:
    - The BetterKeyStore and getKeys lua script still needs to be entered manually into the event
      editor
    - The BetterKeyStore keyvalue is still persistent in the CMOT table
      after calling the CMOT.SAR.packUp() function
    - The CMOT.SAR.packUp() function needs to be updated in the getKeys.lua
      file if any changes occur to the same function within the RunMe.lua
      file

--- CMOT Feature Packs/Downed Unit Creates Survivors (Refactored)/Actions/getKeys.lua
+++ CMOT Feature Packs/Downed Unit Creates Survivors (Refactored)/Actions/getKeys.lua
@@ -2,16 +2,28 @@ CMOT = {}
 CMOT.ScenLoaded = {}
 CMOT.ScenLoaded.countHolders = tonumber(ScenEdit_GetKeyValue("CMOT.ScenLoaded.countHolders"))
 
+
+CMOT.SAR = {}
 CMOT.SAR.component = {}
 function CMOT.SAR.component.count() return #CMOT.SAR.component end
 CMOT.SAR.component.getKeyStores = KeyStore_GetTable("CMOT.SAR.component.getKeyStores")
+CMOT.SAR.component.list = KeyStore_GetTable("CMOT.SAR.component.list")
 
 
 
-CMOT.SAR = {}
+CMOT.SAR.dependency = {}
+CMOT.SAR.dependency.list = KeyStore_GetTable("CMOT.SAR.dependency.list")
+
+
+
+CMOT.SAR.keyStore = {}
+CMOT.SAR.keyStore.list = KeyStore_GetTable("CMOT.SAR.keyStore.list")    -- Structure: {{keyStoreValue}}
+
+
+
 CMOT.SAR.side = {}
-CMOT.SAR.side.searchAndRescue = ScenEdit_GetKeyValue("CMOT.SAR.side.searchAndRescue")
-CMOT.SAR.side.playerSide = ScenEdit_GetKeyValue("CMOT.SAR.side.player")
+CMOT.SAR.side.searchAndRescue = ScenEdit_GetKeyValue("CMOT.SAR.side.searchAndRescue")   -- stores the GUID of the SAR side
+CMOT.SAR.side.playerSide = ScenEdit_GetKeyValue("CMOT.SAR.side.player") -- stores the GUID of the Player side
 
 
 
@@ -30,3 +42,61 @@ CMOT.SAR.wreckage.limit = tonumber(ScenEdit_GetKeyValue("CMOT.SAR.wreckage.limit
     end
 CMOT.SAR.wreckage.list = KeyStore_GetTable("CMO.SAR.survivor.list")
 function CMOT.SAR.wreckage.count() return #CMOT.SAR.wreckage.list end
+
+
+
+-- [#Tracking]  functions to keep updating to any changes
+function CMOT.SAR.component.removeAll()
+    for k, v in pairs(CMOT.SAR.component.list) do
+    end
+    
+    print("Successfully removed all components for the package")
+end
+
+function CMOT.SAR.dependency.releaseAll()
+    print("Successfully released all dependencies values for the package")
+end
+
+function CMOT.SAR.keyStore.clearAll()
+    -- for k, v in pairs() do
+    --     ScenEdit_ClearKeyValue()
+    -- end
+
+    print("Successfully cleared all keystore values for the package")
+end
+
+
+function CMOT.SAR.side.removeAll()
+    function removeSideSAR()
+        local guidSideSAR = ScenEdit_GetKeyValue("CMOT.SAR.side.searchAndRescue")
+        
+        -- when the sideSAR was never defined
+        if (guidSideSAR == "") then return end
+        
+        local side = ScenEdit_RemoveSide({side = guidSideSAR})
+
+        -- when guid exist by side does not exist
+        if(side == nil) then
+            local errorMsg = [[
+            Error: Somehow CMOT.SAR.side.searchAndRescue is present...
+            ...but cannot retrieve side by using guid
+            ]]
+            print(errorMsg)
+            ScenEdit_MsgBox(errorMsg, 6)
+            return
+        end
+        
+        print("Successfully removed side \"" .. side.name .."\"")
+    end
+
+    removeSideSAR()
+    print("Successfully removed all extra sides for the package")
+end
+
+function CMOT.SAR.packUp()
+    CMOT.SAR.side.removeAll()
+    CMOT.SAR.component.removeAll()
+    CMOT.SAR.dependency.releaseAll()
+    CMOT.SAR.keyStore.clearAll()
+    CMOT.SAR = nil
+end
--- CMOT Feature Packs/Downed Unit Creates Survivors (Refactored)/RunMe.lua
+++ CMOT Feature Packs/Downed Unit Creates Survivors (Refactored)/RunMe.lua
@@ -11,6 +11,35 @@
 
 
 
+-- creates a table "CMOT" if not defined yet
+if (CMOT == nil) then
+    CMOT = {}
+end
+
+-- input mode (add pack/delete pack)
+local button = UI_CallAdvancedDialog("Mode Selection", "Select Mode", {"Add SAR Pack", "Delete SAR Pack"})
+if button == "" then return end
+
+if (button == "Delete SAR Pack") then
+    print("Selected \"Delete SAR Pack\"")
+    
+    -- if SAR feature pack was never present
+    if(CMOT.SAR == nil) then return end
+
+    -- when the SAR feature pack was already present
+    CMOT.SAR.packUp()
+    return
+end
+
+
+-- when button == Add SAR Pack
+
+-- if SAR feature pack was already present
+if(CMOT.SAR ~= nil) then CMOT.SAR.packUp() end
+
+-- if SAR feature pack was never present
+if(CMOT.SAR == nil) then CMOT.SAR = {} end
+
 
 
 
@@ -28,7 +57,6 @@ function CMOT.SAR.Init()
 
     CMOT.SAR.keyStore = {}
     CMOT.SAR.keyStore.list = {} -- Structure: {{keyStoreValue}}
-    CMOT.SAR.keyStore.clearAll()
     
     
     
@@ -37,85 +65,213 @@ function CMOT.SAR.Init()
     CMOT.SAR.side.playerSide = ""
 
 
+
     CMOT.SAR.survivor = {}
     CMOT.SAR.survivor.list = {} -- Structure: {{survivor = guidSurvivor, marker = guidMarker}}
     function CMOT.SAR.survivor.count() return #CMOT.SAR.survivor.list end
 
 
-    
+
+    CMOT.SAR.text = {}
+    CMOT.SAR.text.inputPlayerSide = [[]]
+    CMOT.SAR.text.inputAircraftWreckageLimit = [[]]
+    CMOT.SAR.text.recommendations = [[]]
+    CMOT.SAR.text.reloadScenario = [[]]
+
+
+
+
     CMOT.SAR.wreckage = {}
     CMOT.SAR.wreckage.limit = 1
-    CMOT.SAR.wreckage.list = {} -- Strucutre: {{guid = guidWreckage}}
+    CMOT.SAR.wreckage.list = {} -- Structure: {{guid = guidWreckage}}
     function CMOT.SAR.wreckage.count() return #CMOT.SAR.wreckage.list end
 end
+CMOT.SAR.Init()
 
+-- text
+CMOT.SAR.text.inputPlayerSide = [[
+Select Player's Side
+]]
 
+CMOT.SAR.text.inputAircraftWreckageLimit = [[
+Enter the maximum number of Aircraft Wreckage that can be present at a time
 
-function CMOT.SAR.component.removeAll()
-    for k, v in pairs(CMOT.SAR.component.list) do
-        
-    end
-end
+Purpose: This reduces the total number of unnecessary units in a scenario
+]]
 
-function CMOT.SAR.dependency.releaseAll()
-end
+CMOT.SAR.text.recommendations = [[
+Recommended Configuration for SAR Side:
 
-function CMOT.SAR.keyStore.clearAll()
-    -- for k, v in pairs() do
-    --     ScenEdit_ClearKeyValue()
-    -- end
-end
+- Set SAR side's Awareness Level to "Blind", to reduce unnecessary sensor calculations
+
+- Set SAR side as Neutral to all sides to prevent aggression
+
+- Set SAR side's playability as "Computer-only"
+]]
+
+CMOT.SAR.text.reloadScenario = [[
+For the package to take immediate effect:
+- Kindly save and then reload this scenario
+]]
 
 
 function CMOT.SAR.takeInput()
-    --Input for selection of Player Side
-    CMOT.SAR.text.inputPlayerSide = CMOT.SAR.text.inputPlayerSide:gsub("\n        ", "\r\n")
-    local button = UI_CallAdvancedDialog("Side Selection", Text_PlayerSide, AvailableSides)
-    if button == "" then return nil end
-    ScenEdit_SetKeyValue("CMOT_SAR_PlayerSide", button)
-    CMOT_SAR_PlayerSide = ScenEdit_GetKeyValue("CMOT_SAR_PlayerSide")
+    local availableSides = {}
+
+    -- get available sides in the current scenario
+    function getSides()
+        for k,v in pairs(VP_GetSides()) do
+            table.insert(availableSides, v.name)
+        end
+    end
+    
+    -- input for selection of Player Side
+    function inputPlayerSide()
+        -- retrieve input text for player side
+        CMOT.SAR.text.inputPlayerSide = CMOT.SAR.text.inputPlayerSide:gsub("\n        ", "\r\n")
+
+        -- display input text for player side
+        local button = UI_CallAdvancedDialog("Player Side Selection", CMOT.SAR.text.inputPlayerSide, availableSides)
+        if button == "" then return -1 end
+        
+        -- matching the selected side name to its guid
+        for k, side in pairs(VP_GetSides()) do
+            if(button == side.name) then
+                local guidPlayerSide = side.guid
+
+                -- set key
+                ScenEdit_SetKeyValue("CMOT.SAR.side.player", guidPlayerSide)
+                CMOT.SAR.side.player = ScenEdit_GetKeyValue("CMOT.SAR.side.player")
+                break
+            end
+        end
+    end
+
+    
+    -- creates SAR
+    function createSARSide()
+        local sideSAR = ScenEdit_AddSide({side="CMOT SAR"})
+        local guidSARSide = sideSAR.guid
+        ScenEdit_SetKeyValue("CMOT.SAR.side.searchAndRescue", guidSARSide)
+        CMOT.SAR.side.searchAndRescue = ScenEdit_GetKeyValue("CMOT.SAR.side.searchAndRescue")
+    end
+
+    -- input for Aircraft wreckage limit
+    function setAircraftWreckageLimit()
+        while true do
+            CMOT.SAR.text.inputAircraftWreckageLimit = CMOT.SAR.text.inputAircraftWreckageLimit:gsub("\n        ", "\r\n")
+            local input = ScenEdit_InputBox(CMOT.SAR.text.inputAircraftWreckageLimit)
+            
+            if input=="" then
+                ScenEdit_MsgBox("Exiting package creation", 1)
+                return -1
+            end
+            
+            if tonumber(input)==nil then    -- checks if input is NOT a valid number
+                ScenEdit_MsgBox("Input must be a number", 1)
+            else
+                local integer = tonumber(input) - tonumber(input)%1 -- converts possible float value to integer value
+
+                if integer < 0 then -- checks if integer is a logically unacceptable number
+                    ScenEdit_MsgBox("Input must be a number greater than -1", 6)
+                else 
+                    ScenEdit_SetKeyValue("CMOT.SAR.wreckage.limit", tostring(integer))
+                    CMOT.SAR.wreckage.limit = tonumber(ScenEdit_GetKeyValue("CMOT.SAR.wreckage.limit"))
+                    break
+                end
+            end
+        end
+    end
+
+    -- display recommendations
+    function displayRecommendations()
+        CMOT.SAR.text.recommendations = CMOT.SAR.text.recommendations:gsub("\n    ", "\r\n")
+        ScenEdit_MsgBox(CMOT.SAR.text.recommendations, 6)
+    end
+
+    getSides()
+    if inputPlayerSide() == -1 then return -1 end
+    createSARSide()
+    if setAircraftWreckageLimit() == -1 then return -1 end
+    displayRecommendations()
+    return "Input completed"
 end
 
 function CMOT.SAR.buildComponents()
     -- local function bindComponent(event, component)
     
     -- CMOT.SAR.component.getKeyStores = {guid = action.guid, type = "action", CMOT.scenLoaded.event}
+end
+
+function CMOT.SAR.component.removeAll()
+    for k, v in pairs(CMOT.SAR.component.list) do
+    end
     
+    print("Successfully removed all components for the package")
+end
 
+function CMOT.SAR.dependency.releaseAll()
+    print("Successfully released all dependencies values for the package")
+end
+
+function CMOT.SAR.keyStore.clearAll()
+    -- for k, v in pairs() do
+    --     ScenEdit_ClearKeyValue()
+    -- end
+
+    print("Successfully cleared all keystore values for the package")
+end
+
+
+function CMOT.SAR.side.removeAll()
+    function removeSideSAR()
+        local guidSideSAR = ScenEdit_GetKeyValue("CMOT.SAR.side.searchAndRescue")
+        
+        -- when the sideSAR was never defined
+        if (guidSideSAR == "") then return end
+        
+        local side = ScenEdit_RemoveSide({side = guidSideSAR})
+
+        -- when guid exist by side does not exist
+        if(side == nil) then
+            local errorMsg = [[
+            Error: Somehow CMOT.SAR.side.searchAndRescue is present...
+            ...but cannot retrieve side by using guid
+            ]]
+            print(errorMsg)
+            ScenEdit_MsgBox(errorMsg, 6)
+            return
+        end
+        
+        print("Successfully removed side \"" .. side.name .."\"")
+    end
+
+    removeSideSAR()
+    print("Successfully removed all extra sides for the package")
+end
+
+function CMOT.SAR.packUp()
+    CMOT.SAR.side.removeAll()
+    CMOT.SAR.component.removeAll()
+    CMOT.SAR.dependency.releaseAll()
+    CMOT.SAR.keyStore.clearAll()
+    CMOT.SAR = nil
 end
 
 
 
-
-
------<<DRIVER CODE>>-----
-
-if (CMOT == nil) then
-    CMOT = {}
-end
-
-
--- input mode (add pack/delete pack)
-local button = UI_CallAdvancedDialog("Mode Selection", "Select Mode", {"Add SAR Pack", "Delete SAR Pack"})
-if button == "" then return end
-
-
-
-
--- if SAR feature pack was never present
-if (button == "Delete SAR Pack" and CMOT.SAR == nil) then return end
-
-CMOT.SAR.component.removeAll()
-CMOT.SAR.dependency.releaseAll()
-CMOT.SAR.keyStore.clearAll()
-CMOT.SAR = nil
-
-if button == "Delete SAR Pack" then
-    -- is it just better to define the functionallity of "CMOT.packUp" here by itself?
+print("Taking input")
+-- ends the creation of the package if the input was not fully completed
+if CMOT.SAR.takeInput() ~= "Input completed" then
+    CMOT.SAR.packUp()
     return
 end
 
+CMOT.SAR.buildComponents()
 
-CMOT.SAR.Init()
-CMOT.SAR.TakeInput()
+-- request a formal restart/reload to the scenario for immediate effect of the package
+CMOT.SAR.text.reloadScenario = CMOT.SAR.text.reloadScenario:gsub("\n    ", "\r\n")
+ScenEdit_MsgBox(CMOT.SAR.text.reloadScenario, 6)
+
+print("Package was successfully added")
 -------------------------
